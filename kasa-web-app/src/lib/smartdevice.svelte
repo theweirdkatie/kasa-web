19cc7c70f559a3f7caff200192fdd539b6df1b54
theweirdkatie <kstedman.engr@gmail.com>
2023-05-02 17:18:12 -0600

    add smart device lib

---
+++ kasa-web-app/src/lib/smartdevice.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    import { DeskLamp, Power } from 'svelte-materialdesign-icons';
+    import { Button } from 'flowbite-svelte';
+
+    class SmartDevice {
+        host: string;
+        deviceType: number;
+        deviceId: string;
+        name: string;
+        alias: string;
+        mac: string;
+        hasChildren: bool;
+        children: ChildDevice[];
+
+        constructor() {
+            this.host = "192.168.1.207";
+            this.deviceType = 1;
+            this.deviceId = "8006E1AF7F5634FA56B4FD9131F313862053D3BE";
+            this.name = "Smart Wi-Fi Plug Mini";
+            this.alias = "Tank Lamp 1";
+            this.mac = "9C:A2:F4:0C:C5:96";
+            this.hasChildren = false;
+            this.children = [];
+        }
+
+    }
+
+    class ChildDevice {
+        id: string;
+        alias: string;
+    }
+
+    let bulb = new SmartDevice();
+
+    function flip_state() {
+        // console.log("fn");
+        bulb.state = !bulb.state;
+    }
+
+</script>
+
+<div class="flex px-2 gap-2 pt-3.5 place-content-center" id="device1">
+    <div class="flex-none w-20" id="device1icon"><DeskLamp size="50"/></div>
+    <div class="flex-initial w-64 items-center"><span>{bulb.alias}</span></div>
+    <div class="flex-initial w-32"><Button on:click={flip_state} pill={true} outline={!bulb.state} class="!p-2" size="xl"><Power /></Button></div>
+</div>
--- kasa-web-app/src/routes/+page.svelte
+++ kasa-web-app/src/routes/+page.svelte
@@ -1,55 +1,14 @@
 <script lang="ts">
     import { Button } from 'flowbite-svelte';
     import { LightBulb } from 'svelte-heros-v2';
-    import { DeskLamp } from 'svelte-materialdesign-icons';
-    let deviceId = '8006D1848751AA826A0462EAF15DF43F2053448C'
-    let requestData = "{\"system\":{\"set_relay_state\":{\"state\":1}}}"
-    let params = {
-        deviceId,
-        requestData
-    }
+    import SmartDevice from '../lib/smartdevice.svelte';
 
-    let result = ""
-    let _body = ""
-    let device = ""
-    let device_list = []
-
-    async function httpRequest () {
-        let method = 'passthrough'
-        _body = JSON.stringify({
-                        method,
-                        params,
-                    })
-        const res = await fetch('https://use1-wap.tplinkcloud.com/?token=2519c0fa-ATYf4oe7VO8F05tljyAAK67', {
-            method: 'POST',
-            body: JSON.stringify({
-                method,
-                params,
-            })
-        })
-            
-        const json = await res.json()
-        result = JSON.stringify(json)
-    }
-
-    async function getDevices () {
-        let method = 'getDeviceList'
-        _body = JSON.stringify({
-                        method,
-                    })
-        const res = await fetch('https://wap.tplinkcloud.com?token=2519c0fa-ATYf4oe7VO8F05tljyAAK67', {
-            method: 'POST',
-            body: JSON.stringify({
-                method,
-            })
-        })
-            
-        const json = await res.json()
-        result = JSON.stringify(json)
-        device_list = JSON.parse(result).result.deviceList
-        device = device_list[4].alias
-    }
-
+    // let bulb = new SmartDevice();
+    let bulb = {
+        alias: "Tank Lamp 1",
+        state: true,
+    };
+    
 </script>
 
 <div class="flex place-content-center gap-4 mt-6">
@@ -57,8 +16,4 @@
     <Button>Filter</Button>
 </div>
 
-<div class="flex px-2 gap-2 pt-3.5 place-content-center" id="device1">
-    <div class="flex-none w-20" id="device1icon"><DeskLamp /></div>
-    <div class="flex-initial w-64 bg-blue-600">02</div>
-    <div class="flex-initial w-32 bg-green-700">03</div>
-</div>
+<SmartDevice />
