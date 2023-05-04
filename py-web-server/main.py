e88ce4a13cde05f9710c37c77215c84712d37a92
theweirdkatie <kstedman.engr@gmail.com>
2023-05-04 14:06:19 -0600

    Changed api return type to be more json friendly

--- py-web-server/main.py
+++ py-web-server/main.py
@@ -2,9 +2,32 @@ from typing import Union
 import jsonpickle
 from fastapi import FastAPI
 from kasa import Discover, SmartDevice
+from enum import Enum, auto
 
 app = FastAPI()
 
+class DeviceType(Enum):
+  Plug = 1
+  Bulb = auto()
+  Strip = auto()
+  StripSocket = auto()
+  Dimmer = auto()
+  LightStrip = auto()
+  Unknown = -1,
+
+class ChildDevice:
+    id: str
+    alias: str
+
+class SmartDeviceAPI:
+    host: str
+    deviceType: DeviceType
+    deviceId: str
+    name: str
+    alias: str
+    mac: str
+    hasChildren: bool
+    children: list(ChildDevice)
 
 @app.get("/")
 async def root():
@@ -17,4 +40,24 @@ async def list_devices(host: Union[str, None] = None):
         await dev.update()
         return jsonpickle.encode(dev)
     found_devices = await Discover.discover()
-    return jsonpickle.encode(list(found_devices.values()))
+
+    devices = []
+
+    for device in found_devices.values():
+        devices.append(convert_device(device))
+
+    return jsonpickle.encode(devices)
+    
+async def convert_device(device: SmartDevice):
+    d = SmartDeviceAPI
+    d.alias = device.alias
+    d.host = device.host
+    d.deviceId = device.device_id
+    d.deviceType = device.device_type
+    d.hasChildren = len(device.children) > 0
+    if d.hasChildren:
+        for child in device.children:
+            c = convert_device(child)
+            d.children.append(c)
+
+    return d
